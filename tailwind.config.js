49af1a4a08395686b417fa8972dab9299903a8d3
JoelQ <joelquey@hotmail.com>
2021-01-30 19:00:49 +0800

    Updated some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: {
     content: []
@@ -7,6 +9,33 @@ module.exports = {
     fontFamily: {
       sans: "Lato, sans-serif",
       heading: "Playfair Display, serif",
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      bluegray: colors.blueGray,
+      coolgray: colors.coolGray,
+      gray: colors.gray,
+      truegray: colors.trueGray,
+      warmgray: colors.warmGray,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      lightblue: colors.lightBlue,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose
     },
     extend: {
       backgroundImage: theme => ({
