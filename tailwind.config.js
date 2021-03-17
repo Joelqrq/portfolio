33510b42c55925cc63e10a6dc78f955b57171ffd
JoelQ <13463115+Joelqrq@users.noreply.github.com>
2021-03-18 02:16:21 +0800

    Updated text style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
-const colors = require('tailwindcss/colors')
-require('dotenv').config();
+const colors = require("tailwindcss/colors");
+require("dotenv").config();
 const enablePurge = process.env.ENABLE_PURGE || false;
 
 module.exports = {
   purge: {
-    enabled: enablePurge,
-    content: [
-      './src/**/*.html',
-      './src/**/*.scss'
-    ]
+    //enabled: enablePurge,
+    content: ["./src/**/*.html", "./src/**/*.scss"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -17,8 +14,8 @@ module.exports = {
       heading: "Playfair Display, serif",
     },
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      transparent: "transparent",
+      current: "currentColor",
       black: colors.black,
       white: colors.white,
       bluegray: colors.blueGray,
@@ -41,12 +38,11 @@ module.exports = {
       violet: colors.violet,
       fuchsia: colors.fuchsia,
       pink: colors.pink,
-      rose: colors.rose
+      rose: colors.rose,
     },
     extend: {
-      backgroundImage: theme => ({
-      })
-    }
+      backgroundImage: (theme) => ({}),
+    },
   },
   variants: {
     extend: {},
